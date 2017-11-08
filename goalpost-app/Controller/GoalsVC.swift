5995a0be5b7f4287994c6a534eaaf58cacfe9e36
Sercan <sercanorhangazi@gmail.com>
2017-11-08 21:59:16 +0300

    Building GoalsVc

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by Sercan on 8.11.2017.
+//  Copyright © 2017 Sercan Orhangazi. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addGoalButtonPressed(_ sender: Any) {
+    }
+    
+}
+
