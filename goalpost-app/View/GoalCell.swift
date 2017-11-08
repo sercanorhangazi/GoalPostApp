317d7dd0975fa23150e862573107dd7ba3692d3f
Sercan <sercanorhangazi@gmail.com>
2017-11-08 22:32:21 +0300

    Building GoalCell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Sercan on 8.11.2017.
+//  Copyright © 2017 Sercan Orhangazi. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTermLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    func configureCell(description : String, type : String, goalProgressAmount : Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTermLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressLabel)
+    }
+
+}
