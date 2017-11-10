245c4c780bf13ff3689cdfafcbae5a4bac9ade5c
Sercan <sercanorhangazi@gmail.com>
2017-11-10 19:45:07 +0300

    Saving Goal Data to Persistent Store

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -30,10 +33,35 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
         pointsTextField.delegate = self
     }
     
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
     @IBAction func doneButtonPressed(_ sender: Any) {
-        guard let textFieldValue = pointsTextField.text else { return }
-        guard let completionPoints = Int(textFieldValue) else { return }
+        if pointsTextField.text != "" {
+            self.save { (success) in
+                dismiss(animated: true, completion: nil)
+            }
+        }
         
+    }
+    
+    func save(completion : (_ finished : Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgressValue = Int32(0)
+        
+        do {
+            try managedContext.save()
+            completion(true)
+        } catch {
+            debugPrint("Couldn't save : \(error.localizedDescription)")
+            completion(false)
+        }
     }
     
     
